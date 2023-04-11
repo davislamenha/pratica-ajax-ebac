97a0eb1ee344e6b31c66a599727d07442ad9c893
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-04-11 06:57:50 -0300

    requisiçao com jquery

--- main.js
+++ main.js
@@ -10,3 +10,20 @@
 //       xhttp.send();
 //     });
 // });
+
+$(document).ready(function () {
+  $('#btn-buscar-cep').on('click', function () {
+    const cep = $('#cep').val();
+    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+    $.ajax(endpoint).done(function (resposta) {
+      const logradouro = resposta.logradouro;
+      const bairro = resposta.bairro;
+      const cidade = resposta.localidade;
+      const estado = resposta.uf;
+      const endereco = `${logradouro}, ${bairro}, ${cidade}-${estado}`;
+
+      $('#endereco').val(endereco);
+    });
+  });
+});
