ccfd125557eb594ee85fc7571d72bda777b7fb42
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-04-12 07:24:12 -0300

    requisição com fetch

--- main.js
+++ main.js
@@ -12,23 +12,42 @@
 // });
 
 $(document).ready(function () {
+  $('#cep').mask('00000-000');
+
   $('#btn-buscar-cep').on('click', function () {
     const cep = $('#cep').val();
     const endpoint = `https://viacep.com.br/ws/${cep}/json`;
     $(this).find('i').addClass('d-none');
     $(this).find('span').removeClass('d-none');
 
-    $.ajax(endpoint).done(function (resposta) {
-      const logradouro = resposta.logradouro;
-      const bairro = resposta.bairro;
-      const cidade = resposta.localidade;
-      const estado = resposta.uf;
-      const endereco = `${logradouro}, ${bairro}, ${cidade}-${estado}`;
+    // $.ajax(endpoint).done(function (resposta) {
+    //   const logradouro = resposta.logradouro;
+    //   const bairro = resposta.bairro;
+    //   const cidade = resposta.localidade;
+    //   const estado = resposta.uf;
+    //   const endereco = `${logradouro}, ${bairro}, ${cidade}-${estado}`;
 
-      $('#endereco').val(endereco);
+    //   $('#endereco').val(endereco);
 
-      $('#btn-buscar-cep').find('span').addClass('d-none');
-      $('#btn-buscar-cep').find('i').removeClass('d-none');
-    });
+    //   $('#btn-buscar-cep').find('span').addClass('d-none');
+    //   $('#btn-buscar-cep').find('i').removeClass('d-none');
+    // });
+
+    fetch(endpoint)
+      .then(function (resposta) {
+        return resposta.json();
+      })
+      .then(function (json) {
+        const logradouro = json.logradouro;
+        const bairro = json.bairro;
+        const cidade = json.localidade;
+        const estado = json.uf;
+        const endereco = `${logradouro}, ${bairro}, ${cidade}-${estado}`;
+
+        $('#endereco').val(endereco);
+
+        $('#btn-buscar-cep').find('span').addClass('d-none');
+        $('#btn-buscar-cep').find('i').removeClass('d-none');
+      });
   });
 });
